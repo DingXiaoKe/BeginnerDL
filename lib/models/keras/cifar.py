6b017c133c6bee97529d4c3a0df1dbf9cded21f8
JJZHK <jeffcobile@gmail.com>
2018-03-10 06:27:33 +0800

    基础库重构

--- lib/models/keras/cifar.py
+++ lib/models/keras/cifar.py
@@ -1,260 +1,132 @@
 from keras.layers import Input, Conv2D, MaxPooling2D, BatchNormalization,Activation,\
-    Flatten,Dense,Dropout,Add,AveragePooling2D,GlobalAveragePooling2D
+    Flatten,Dense,Dropout,GlobalAveragePooling2D,AveragePooling2D,ZeroPadding2D,Concatenate, \
+    Reshape
 from keras.models import Model
-
 from lib.config.cifarConfig import Cifar10Config
+import numpy as np
 
-# from keras_layers.resnet import resnet_block
-# from keras_layers.senet import residual_layer
-# from keras_layers.densenet import dense_block,transition_block
-# from keras.regularizers import l2
-cfg = Cifar10Config()
+class AlexNet(object):
+    def __init__(self, config=Cifar10Config()):
+        self.cfg = config
 
-def alexnet():
-    image_input = Input(shape=(cfg.IMAGE_SIZE, cfg.IMAGE_SIZE, cfg.IMAGE_CHANNEL))
-    network = Conv2D(filters=96, kernel_size=(5,5), kernel_initializer="he_normal", padding="same")(image_input)
-    network = BatchNormalization()(network)
-    network = Activation('relu')(network)
-    network = MaxPooling2D(pool_size=(3,3), strides=(2,2), padding="same")(network)
+    def network(self):
+        image_input = Input(shape=(self.cfg.IMAGE_SIZE, self.cfg.IMAGE_SIZE, self.cfg.IMAGE_CHANNEL))
+        network = Conv2D(filters=96, kernel_size=(5,5), kernel_initializer="he_normal", padding="same")(image_input)
+        network = BatchNormalization()(network)
+        network = Activation('relu')(network)
+        network = MaxPooling2D(pool_size=(3,3), strides=(2,2), padding="same")(network)
 
-    network = Conv2D(filters=256, kernel_size=(5,5), padding="same", kernel_initializer="he_normal")(network)
-    network = BatchNormalization()(network)
-    network = Activation('relu')(network)
-    network = MaxPooling2D(pool_size=(3,3), strides=(2,2), padding="same")(network)
+        network = Conv2D(filters=256, kernel_size=(5,5), padding="same", kernel_initializer="he_normal")(network)
+        network = BatchNormalization()(network)
+        network = Activation('relu')(network)
+        network = MaxPooling2D(pool_size=(3,3), strides=(2,2), padding="same")(network)
 
-    network = Conv2D(filters=384, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
-    network = BatchNormalization()(network)
-    network = Activation('relu')(network)
+        network = Conv2D(filters=384, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
+        network = BatchNormalization()(network)
+        network = Activation('relu')(network)
 
-    network = Conv2D(filters=384, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
-    network = BatchNormalization()(network)
-    network = Activation('relu')(network)
+        network = Conv2D(filters=384, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
+        network = BatchNormalization()(network)
+        network = Activation('relu')(network)
 
-    network = Conv2D(filters=256, kernel_size=(3,3), padding="same",kernel_initializer="he_normal")(network)
-    network = BatchNormalization()(network)
-    network = Activation('relu')(network)
-    network = MaxPooling2D(pool_size=(3,3), strides=(2,2), padding="same")(network)
+        network = Conv2D(filters=256, kernel_size=(3,3), padding="same",kernel_initializer="he_normal")(network)
+        network = BatchNormalization()(network)
+        network = Activation('relu')(network)
+        network = MaxPooling2D(pool_size=(3,3), strides=(2,2), padding="same")(network)
 
-    network = Flatten()(network)
-    network = Dense(units=4096)(network)
-    network = BatchNormalization()(network)
-    network = Activation('relu')(network)
-    network = Dropout(rate=0.5)(network)
+        network = Flatten()(network)
+        network = Dense(units=4096)(network)
+        network = BatchNormalization()(network)
+        network = Activation('relu')(network)
+        network = Dropout(rate=0.5)(network)
 
-    network = Dense(units=4096)(network)
-    network = BatchNormalization()(network)
-    network = Activation('relu')(network)
-    network = Dropout(rate=0.5)(network)
+        network = Dense(units=4096)(network)
+        network = BatchNormalization()(network)
+        network = Activation('relu')(network)
+        network = Dropout(rate=0.5)(network)
 
-    network = Dense(units=cfg.NUM_OUTPUTS, activation="softmax")(network)
+        network = Dense(units=self.cfg.NUM_OUTPUTS, activation="softmax")(network)
 
-    model = Model(inputs=image_input, outputs=network)
-    return model
+        model = Model(inputs=image_input, outputs=network)
+        return model
 
-# def vgg19():
-#     image_input = Input(shape=(cfg.IMAGE_SIZE, cfg.IMAGE_SIZE, cfg.IMAGE_CHANNEL))
-#     network = Conv2D(filters=64, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(image_input)
-#     network = BatchNormalization()(network)
-#     network = Activation('relu')(network)
-#     network = Conv2D(filters=64, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
-#     network = BatchNormalization()(network)
-#     network = Activation('relu')(network)
-#     network = MaxPooling2D(pool_size=(2,2), strides=(2,2), padding="same")(network)
-#
-#     network = Conv2D(filters=128, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
-#     network = BatchNormalization()(network)
-#     network = Activation('relu')(network)
-#     network = Conv2D(filters=128, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
-#     network = BatchNormalization()(network)
-#     network = Activation('relu')(network)
-#     network = MaxPooling2D(pool_size=(2,2), strides=(2,2), padding="same")(network)
-#
-#     network = Conv2D(filters=256, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
-#     network = BatchNormalization()(network)
-#     network = Activation('relu')(network)
-#     network = Conv2D(filters=256, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
-#     network = BatchNormalization()(network)
-#     network = Activation('relu')(network)
-#     network = Conv2D(filters=256, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
-#     network = BatchNormalization()(network)
-#     network = Activation('relu')(network)
-#     network = Conv2D(filters=256, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
-#     network = BatchNormalization()(network)
-#     network = Activation('relu')(network)
-#     network = MaxPooling2D(pool_size=(2,2), strides=(2,2), padding="same")(network)
-#
-#     network = Conv2D(filters=512, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
-#     network = BatchNormalization()(network)
-#     network = Activation('relu')(network)
-#     network = Conv2D(filters=512, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
-#     network = BatchNormalization()(network)
-#     network = Activation('relu')(network)
-#     network = Conv2D(filters=512, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
-#     network = BatchNormalization()(network)
-#     network = Activation('relu')(network)
-#     network = Conv2D(filters=512, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
-#     network = BatchNormalization()(network)
-#     network = Activation('relu')(network)
-#     network = MaxPooling2D(pool_size=(2,2), strides=(2,2), padding="same")(network)
-#
-#     network = Conv2D(filters=512, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
-#     network = BatchNormalization()(network)
-#     network = Activation('relu')(network)
-#     network = Conv2D(filters=512, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
-#     network = BatchNormalization()(network)
-#     network = Activation('relu')(network)
-#     network = Conv2D(filters=512, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
-#     network = BatchNormalization()(network)
-#     network = Activation('relu')(network)
-#     network = Conv2D(filters=512, kernel_size=(3,3), padding="same", kernel_initializer="he_normal")(network)
-#     network = BatchNormalization()(network)
-#     network = Activation('relu')(network)
-#
-#     network = Flatten()(network)
-#     network = Dense(units=4096)(network)
-#     network = BatchNormalization()(network)
-#     network = Activation('relu')(network)
-#     network = Dropout(rate=0.5)(network)
-#
-#     network = Dense(units=4096)(network)
-#     network = BatchNormalization()(network)
-#     network = Activation('relu')(network)
-#     network = Dropout(rate=0.5)(network)
-#
-#     network = Dense(units=cfg.NUM_OUTPUTS)(network)
-#
-#     model = Model(inputs=image_input, outputs=network)
-#     return model
-#
-# def resnet_V1(depth):
-#     inputs = Input(shape=(cfg.IMAGE_SIZE, cfg.IMAGE_SIZE, cfg.IMAGE_CHANNEL))
-#
-#     if (depth - 2) % 6 != 0:
-#         raise ValueError('depth should be 6n+2 (eg 20, 32, 44 in [a])')
-#
-#     num_filters = 16
-#     num_sub_blocks = int((depth - 2) / 6)
-#
-#     x = resnet_block(inputs=inputs)
-#     # Instantiate convolutional base (stack of blocks).
-#     for i in range(3):
-#         for j in range(num_sub_blocks):
-#             strides = 1
-#             is_first_layer_but_not_first_block = j == 0 and i > 0
-#             if is_first_layer_but_not_first_block:
-#                 strides = 2
-#             y = resnet_block(inputs=x,
-#                              num_filters=num_filters,
-#                              strides=strides)
-#             y = resnet_block(inputs=y,
-#                              num_filters=num_filters,
-#                              activation=None)
-#             if is_first_layer_but_not_first_block:
-#                 x = resnet_block(inputs=x,
-#                                  num_filters=num_filters,
-#                                  kernel_size=1,
-#                                  strides=strides,
-#                                  activation=None)
-#             x = Add()([x, y])
-#             x = Activation('relu')(x)
-#         num_filters = 2 * num_filters
-#
-#     # Add classifier on top.
-#     # v1 does not use BN after last shortcut connection-ReLU
-#     x = AveragePooling2D(pool_size=8)(x)
-#     y = Flatten()(x)
-#     outputs = Dense(cfg.NUM_OUTPUTS,
-#                     activation='softmax',
-#                     kernel_initializer='he_normal')(y)
-#
-#     # Instantiate model.
-#     model = Model(inputs=inputs, outputs=outputs)
-#     return model
-#
-# def resnet_V2(depth):
-#     inputs = Input(shape=(cfg.IMAGE_SIZE, cfg.IMAGE_SIZE, cfg.IMAGE_CHANNEL))
-#     if (depth - 2) % 6 != 0:
-#         raise ValueError('depth should be 6n+2 (eg 20, 32, 44 in [a])')
-#
-#     num_filters = 16
-#     num_sub_blocks = int((depth - 2) / 6)
-#
-#     x = resnet_block(inputs=inputs)
-#     # Instantiate convolutional base (stack of blocks).
-#     for i in range(3):
-#         for j in range(num_sub_blocks):
-#             strides = 1
-#             is_first_layer_but_not_first_block = j == 0 and i > 0
-#             if is_first_layer_but_not_first_block:
-#                 strides = 2
-#             y = resnet_block(inputs=x,
-#                              num_filters=num_filters,
-#                              strides=strides)
-#             y = resnet_block(inputs=y,
-#                              num_filters=num_filters,
-#                              activation=None)
-#             if is_first_layer_but_not_first_block:
-#                 x = resnet_block(inputs=x,
-#                                  num_filters=num_filters,
-#                                  kernel_size=1,
-#                                  strides=strides,
-#                                  activation=None)
-#             x = Add()([x, y])
-#             x = Activation('relu')(x)
-#         num_filters = 2 * num_filters
-#
-#     # Add classifier on top.
-#     # v1 does not use BN after last shortcut connection-ReLU
-#     x = AveragePooling2D(pool_size=8)(x)
-#     y = Flatten()(x)
-#     outputs = Dense(cfg.NUM_OUTPUTS,
-#                     activation='softmax',
-#                     kernel_initializer='he_normal')(y)
-#
-#     # Instantiate model.
-#     model = Model(inputs=inputs, outputs=outputs)
-#     return model
-#
-# def densenet(depth=40, nb_dense_block=3, growth_rate=12, nb_filter=16, dropout_rate=None,
-#                    weight_decay=1E-4, verbose=True):
-#     model_input = Input(shape=(cfg.IMAGE_SIZE,cfg.IMAGE_SIZE,cfg.IMAGE_CHANNEL))
-#
-#     concat_axis = 3
-#     assert (depth - 4) % 3 == 0, "Depth must be 3 N + 4"
-#     nb_layers = int((depth - 4) / 3)
-#
-#     # Initial convolution
-#     x = Conv2D(filters=nb_filter, kernel_size=(3, 3), kernel_initializer="he_uniform", padding="same", name="initial_conv2D", use_bias=False,
-#                       kernel_regularizer=l2(weight_decay))(model_input)
-#     x = BatchNormalization(axis=concat_axis, gamma_regularizer=l2(weight_decay),
-#                            beta_regularizer=l2(weight_decay))(x)
-#     x = Activation('relu')(x)
-#
-#     for block_idx in range(nb_dense_block - 1):
-#         x, nb_filter = dense_block(x, nb_layers, nb_filter, growth_rate, dropout_rate=dropout_rate,
-#                                    weight_decay=weight_decay)
-#         x = transition_block(x, nb_filter, dropout_rate=dropout_rate, weight_decay=weight_decay)
-#
-#     x, nb_filter = dense_block(x, nb_layers, nb_filter, growth_rate, dropout_rate=dropout_rate,
-#                                weight_decay=weight_decay)
-#     x = Activation('relu')(x)
-#     x = GlobalAveragePooling2D()(x)
-#     x = Dense(cfg.NUM_OUTPUTS, activation='softmax', kernel_regularizer=l2(weight_decay), bias_regularizer=l2(weight_decay))(x)
-#
-#     densenet = Model(inputs=model_input, outputs=x)
-#     return densenet
-#
-# def senet():
-#     image_input = Input(shape=(cfg.IMAGE_SIZE, cfg.IMAGE_SIZE, cfg.IMAGE_CHANNEL))
-#     network = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding="SAME")(image_input)
-#     network = BatchNormalization(trainable=True)(network)
-#     network = Activation('relu')(network)
-#     network = residual_layer(network, out_dim=64, training=True)
-#     network = residual_layer(network, out_dim=128, training=True)
-#     network = residual_layer(network, out_dim=256, training=True)
-#     network = GlobalAveragePooling2D()(network)
-#     network = Flatten()(network)
-#     network = Dense(units=cfg.NUM_OUTPUTS, activation="softmax")(network)
-#
-#     model = Model(inputs=image_input, outputs=network)
-#     return model
+class SENet(object):
+    def __init__(self, cfg = Cifar10Config()):
+        self.cfg = cfg
+
+    def network(self):
+        image_input = Input(shape=(self.cfg.IMAGE_SIZE, self.cfg.IMAGE_SIZE, self.cfg.IMAGE_CHANNEL))
+        network = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding="SAME")(image_input)
+        network = BatchNormalization(trainable=True)(network)
+        network = Activation('relu')(network)
+        network = self._residual_layer(network, out_dim=64, training=True)
+        network = self._residual_layer(network, out_dim=128, training=True)
+        network = self._residual_layer(network, out_dim=256, training=True)
+        network = GlobalAveragePooling2D()(network)
+        network = Flatten()(network)
+        network = Dense(units=self.cfg.NUM_OUTPUTS, activation="softmax")(network)
+
+        model = Model(inputs=image_input, outputs=network)
+        return model
+
+    def _residual_layer(self, input_x, out_dim, training, res_block=3, reduction_ratio=4):
+        for i in range(res_block):
+            input_dim = int(np.shape(input_x)[-1])
+
+            if input_dim * 2 == out_dim:
+                flag = True
+                stride = 2
+                channel = input_dim // 2
+            else:
+                flag = False
+                stride = 1
+
+            x = self._split_layer(input_x, training=training, stride=stride)
+            x = self._transition_layer(x, training=training, out_dim=out_dim)
+            x = self._squeeze_excitation_layer(x, out_dim=out_dim, ratio=reduction_ratio)
+
+            if flag is True :
+                pad_input_x = AveragePooling2D(padding="SAME")(input_x)
+                # pad_input_x = tf.pad(pad_input_x, [[0, 0], [0, 0], [0, 0], [channel, channel]])
+                pad_input_x = ZeroPadding2D(padding=((0,0), (0, channel)))(pad_input_x)
+            else :
+                pad_input_x = input_x
+
+            input_x = Activation('relu')(x + pad_input_x)
+        return x
+
+    def _split_layer(self,input_x, training,  stride, cardinality=8):
+        layers_list = list()
+
+        for i in range(cardinality):
+            splits = self._transform_layer(input_x, training=training, stride=stride)
+            layers_list.append(splits)
+
+        return Concatenate()(layers_list)
+
+    def _transition_layer(self, x, training, out_dim):
+        x = Conv2D(filters=out_dim, kernel_size=(1,1), strides=(1,1), padding="SAME")(x)
+        x = BatchNormalization(trainable=training)(x)
+
+        return x
+
+    def _squeeze_excitation_layer(self,input_x, out_dim, ratio):
+        squeeze = GlobalAveragePooling2D()(input_x)
+        excitation = Dense(units=out_dim / ratio)(squeeze)
+        excitation = Activation('relu')(excitation)
+        excitation = Dense(units=out_dim)(excitation)
+        excitation = Activation('sigmoid')(excitation)
+        excitation = Reshape([-1,1,1,out_dim])(excitation)
+        scale = input_x * excitation
+
+        return scale
+
+    def _transform_layer(self, x, training, stride, depth = 8):
+        x = Conv2D(filters=depth, kernel_size=(1,1), strides=(1,1), padding="SAME")(x)
+        x = BatchNormalization(trainable=training)(x)
+        x = Activation('relu')(x)
+        x = Conv2D(filters=depth, kernel_size=(3,3), strides=stride, padding="SAME")(x)
+        x = BatchNormalization(trainable=training)(x)
+        x = Activation('relu')(x)
+
+        return x
