5ad40d558404c5ec23064f56640f8cf0f356f013
JJZHK <jeffcobile@gmail.com>
2018-03-10 13:52:13 +0800

    图像分类

--- 02.01.object_classification/keras_network.py
+++ 02.01.object_classification/keras_network.py
@@ -1,22 +1,49 @@
 import tensorflow as tf
 import math
+import scipy
 
 import keras.backend as K
-from keras.losses import categorical_crossentropy
-from keras.optimizers import SGD
-from keras.utils import to_categorical
 from keras.utils import multi_gpu_model
 from keras.callbacks import EarlyStopping, ModelCheckpoint, LearningRateScheduler
+from keras.optimizers import SGD
+from keras.losses import categorical_crossentropy
+from keras.utils import to_categorical
+
 from keras.preprocessing.image import ImageDataGenerator
 
 from lib.utils.progressbar.keras.ProgressBarCallback import ProgressBar
 from lib.datareader.DataReaderForClassification import DataReader
-from lib.models.keras.cifar import SENet
 from lib.config.cifarConfig import Cifar10Config
+from lib.models.keras.cifar import AlexNet,Vgg19,SENet,DenseNet,ResNet
 
-GPU_NUM = 2
-EPOCH_NUM = 100
 cfg = Cifar10Config()
+MODEL = "alexnet"
+
+MODEL_LIST = [
+    {
+        "name" : "alexnet",
+        "model" : AlexNet(cfg)
+    },
+    {
+        "name" : "Vgg19",
+        "model" : Vgg19(cfg, zoom=2)
+    },
+    {
+        "name" : "SENet",
+        "model" : SENet(cfg)
+    },
+    {
+        "name" : "ResNet",
+        "model" : ResNet(cfg, zoom=7)
+    },
+    {
+        "name" : "DenseNet",
+        "model" : DenseNet(cfg, zoom=1)
+    }
+]
+
+GPU_NUM = 1
+EPOCH_NUM = 100
 
 def lr_schedule(epoch):
     initial_lrate = 0.01
@@ -27,7 +54,10 @@ def lr_schedule(epoch):
 
 session = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
 K.set_session(session)
-model = SENet(cfg).network()
+model = MODEL_LIST["name"==MODEL]["model"]
+zoom = model.zoom
+print(zoom)
+model = model.network()
 
 if GPU_NUM >= 2:
     model = multi_gpu_model(model, gpus=GPU_NUM)
@@ -43,23 +73,25 @@ train_dataGen = ImageDataGenerator(
     zoom_range=0.2,
     horizontal_flip=True)
 
-train_generator = train_dataGen.flow_from_directory(directory="../data/cifar10/train", target_size=(cfg.IMAGE_SIZE,cfg.IMAGE_SIZE),
-                                                    batch_size=cfg.BATCH_SIZE,
-                                  class_mode='categorical')
+train_generator = train_dataGen.flow_from_directory(directory="../data/cifar10/train",
+                                                    target_size=(cfg.IMAGE_SIZE*zoom,cfg.IMAGE_SIZE*zoom), batch_size=cfg.BATCH_SIZE,
+                                                    class_mode='categorical')
 probar = ProgressBar()
 es = EarlyStopping(monitor='val_acc', patience=EPOCH_NUM)
-checkpoint = ModelCheckpoint(filepath="cifar10_alexnet.h5", save_best_only=True, save_weights_only=True)
+checkpoint = ModelCheckpoint(filepath="cifar10_{%s}.h5" % MODEL, save_best_only=True, save_weights_only=True)
 lrate = LearningRateScheduler(lr_schedule)
 
 reader = DataReader()
 x_test, y_test = reader.readData(phrase="test")
-y_test = to_categorical(y_test, num_classes=10)
+y_test = to_categorical(y_test, num_classes=cfg.NUM_OUTPUTS)
+x_test = scipy.misc.imresize(x_test, cfg.IMAGE_SIZE * zoom, interp='cubic')
 
-model.fit_generator(generator=train_generator, steps_per_epoch=50000/100,
+model.fit_generator(generator=train_generator, steps_per_epoch=cfg.TRAIN_RECORDS/cfg.BATCH_SIZE,
                     epochs=EPOCH_NUM, verbose=0,
-                    validation_data=(x_test, y_test),validation_steps=10000/100,
+                    validation_data=(x_test, y_test),validation_steps=cfg.TEST_RECORDS/cfg.BATCH_SIZE,
                     callbacks=[probar,es,checkpoint, lrate])
 
 # scores = model.evaluate(x_test, y_test, verbose=1, steps=EPOCH_NUM)
 # print('Test loss:', scores[0])
 # print('Test accuracy:', scores[1])
+
