845a35626937855c8e28af398aa83ebe2080a53a
JJZHK <jeffcobile@gmail.com>
2018-03-27 15:11:54 +0800

    条件生成对抗网络

--- 07.03.cGAN/keras_01.mnist.py
+++ 07.03.cGAN/keras_01.mnist.py
@@ -1,194 +1,161 @@
-from __future__ import print_function, division
-
-from lib.datareader.common import read_mnist
-from keras.layers import Input, Dense, Reshape, Flatten, Dropout, multiply
-from keras.layers import BatchNormalization, Activation, Embedding, ZeroPadding2D
-from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.convolutional import UpSampling2D, Conv2D
-from keras.models import Sequential, Model
-from keras.optimizers import Adam
-
-import matplotlib.pyplot as plt
-plt.switch_backend('agg')
+# coding=utf-8
 import numpy as np
 
+from matplotlib import pyplot as plt
+from keras.optimizers import Adam
+from keras.layers import Input, Dense,LeakyReLU,BatchNormalization, Reshape, Embedding, Flatten, multiply, Dropout
+from keras.models import Sequential, Model
+
+from lib.datareader.common import read_mnist
+from lib.utils.progressbar.ProgressBar import ProgressBar
 from lib.config.mnist import MNISTConfig
+
 config = MNISTConfig()
+EPOCH = 10000
+GPU_NUMS = 1
+BATCH_SIZE = config.BATCH_SIZE
+NOISE_DIM = 100
 
-class CGAN():
-    def __init__(self):
-        self.img_shape = (config.IMAGE_SIZE, config.IMAGE_SIZE, config.IMAGE_CHANNEL)
-        self.num_classes = config.NUM_OUTPUTS
-        self.latent_dim = 100
+'''
+定义Generator网络, 输入为NOISE_DIM,输出为(28,28,1)
+'''
+def builder_generator():
+    image_shape = (config.IMAGE_SIZE, config.IMAGE_SIZE, config.IMAGE_CHANNEL)
+    model = Sequential()
+    model.add(Dense(units=256, input_dim=NOISE_DIM))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(BatchNormalization(momentum=0.8))
 
-        optimizer = Adam(0.0002, 0.5)
+    model.add(Dense(units=512))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(BatchNormalization(momentum=0.8))
 
-        # Build and compile the discriminator
-        self.discriminator = self.build_discriminator()
-        self.discriminator.compile(loss=['binary_crossentropy'],
-                                   optimizer=optimizer,
-                                   metrics=['accuracy'])
+    model.add(Dense(units=1024))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(BatchNormalization(momentum=0.8))
 
-        # Build and compile the generator
-        self.generator = self.build_generator()
-        self.generator.compile(loss=['binary_crossentropy'],
-                               optimizer=optimizer)
+    model.add(Dense(units=np.prod(image_shape), activation='tanh'))
+    model.add(Reshape(image_shape))
 
-        # The generator takes noise and the target label as input
-        # and generates the corresponding digit of that label
-        noise = Input(shape=(100,))
-        label = Input(shape=(1,))
-        img = self.generator([noise, label])
+    noise = Input(shape=(NOISE_DIM,))
+    label = Input(shape=(1,), dtype='int32')
 
-        # For the combined model we will only train the generator
-        self.discriminator.trainable = False
+    embed = Embedding(config.NUM_OUTPUTS, NOISE_DIM)(label)
+    label_embedding = Flatten()(embed)
 
-        # The discriminator takes generated image as input and determines validity
-        # and the label of that image
-        valid = self.discriminator([img, label])
+    model_input = multiply([noise, label_embedding])
 
-        # The combined model  (stacked generator and discriminator) takes
-        # noise as input => generates images => determines validity
-        self.combined = Model([noise, label], valid)
-        self.combined.compile(loss=['binary_crossentropy'],
-                              optimizer=optimizer)
+    model_output = model(model_input)
 
-    def build_generator(self):
+    return Model([noise, label], model_output)
 
-        model = Sequential()
+'''
+定义Discriminator网络
+'''
+def builder_discriminator():
+    image_shape = (config.IMAGE_SIZE, config.IMAGE_SIZE, config.IMAGE_CHANNEL)
+    model = Sequential()
 
-        model.add(Dense(256, input_dim=self.latent_dim))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(Dense(512))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(Dense(1024))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(Dense(np.prod(self.img_shape), activation='tanh'))
-        model.add(Reshape(self.img_shape))
+    model.add(Dense(units=512, input_dim=(np.prod(image_shape))))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dense(units=512))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(rate=0.4))
+    model.add(Dense(units=512))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(rate=0.4))
 
-        model.summary()
+    model.add(Dense(1, activation='sigmoid'))
 
-        noise = Input(shape=(self.latent_dim,))
-        label = Input(shape=(1,), dtype='int32')
+    img = Input(shape=image_shape)
+    label = Input(shape=(1,), dtype='int32')
 
-        embed = Embedding(self.num_classes, self.latent_dim)(label)
-        label_embedding = Flatten()(embed)
+    flatten_label = Flatten()(Embedding(config.NUM_OUTPUTS, np.prod(image_shape))(label))
+    flatten_img = Flatten()(img)
 
-        model_input = multiply([noise, label_embedding])
+    model_input = multiply([flatten_img, flatten_label])
 
-        img = model(model_input)
+    model_output = model(model_input)
 
-        return Model([noise, label], img)
+    return Model([img, label], model_output)
 
-    def build_discriminator(self):
+def save_images(generator):
+    noise = np.random.normal(0, 1, (2 * 5, NOISE_DIM))
+    label = np.arange(0, 10).reshape(-1, 1)
 
-        model = Sequential()
+    generate_image = generator.predict([noise, label])
 
-        model.add(Dense(512, input_dim=np.prod(self.img_shape)))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dense(512))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dropout(0.4))
-        model.add(Dense(512))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dropout(0.4))
-        model.add(Dense(1, activation='sigmoid'))
-        model.summary()
+    gen_imgs = 0.5 * generate_image + 0.5
 
-        img = Input(shape=self.img_shape)
-        label = Input(shape=(1,), dtype='int32')
+    fig, axs = plt.subplots(2, 5)
+    fig.suptitle("CGAN: Generated digits", fontsize=12)
+    cnt = 0
+    for i in range(2):
+        for j in range(5):
+            axs[i,j].imshow(gen_imgs[cnt,:,:,0], cmap='gray')
+            axs[i,j].set_title("Digit: %d" % label[cnt])
+            axs[i,j].axis('off')
+            cnt += 1
+    fig.savefig("output/%d.png" % epoch)
+    plt.close()
 
-        label_embedding = Flatten()(Embedding(self.num_classes, np.prod(self.img_shape))(label))
-        flat_img = Flatten()(img)
+'''
+开始训练
+'''
+'''
+1. 读入数据,然后归一化到(-1,1)
+'''
+(x_train, y_train), (_,_) = read_mnist("../ganData/mnist.npz")
+x_train = (x_train.astype(np.float32) - 127.5) / 127.5 # 127.5 = 255 / 2
+x_train = np.expand_dims(x_train, axis=3) # shape从(60000,28,28) => (60000,28,28,1)
+y_train = np.reshape(y_train, newshape=(-1,1))
 
-        model_input = multiply([flat_img, label_embedding])
+'''
+2. 生成网络
+'''
+Generator = builder_generator()
+Discriminator = builder_discriminator()
+optimizer = Adam(0.0002, 0.5)
+Generator.compile(loss='binary_crossentropy', optimizer=optimizer)
+Discriminator.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+noise_temp = Input(shape=(NOISE_DIM,))
+label_temp = Input(shape=(1,))
+img = Generator([noise_temp, label_temp])
+Discriminator.trainable = False
 
-        validity = model(model_input)
+valid = Discriminator([img, label_temp])
+Gan = Model([noise_temp, label_temp], valid)
+Gan.compile(loss='binary_crossentropy', optimizer=optimizer)
 
-        return Model([img, label], validity)
+'''
+开始训练
+'''
+half_batch = int(BATCH_SIZE / 2)
+proBar = ProgressBar(1, EPOCH, "D loss: %f, acc.: %.2f%%; G loss: %f")
+for epoch in range(1, EPOCH + 1):
+    idx = np.random.randint(0, x_train.shape[0], size=half_batch)
+    image, label = x_train[idx], y_train[idx]
 
-    def train(self, epochs, batch_size=128, save_interval=50):
+    noise = np.random.normal(0, 1, (half_batch, 100))
+    generate_image = Generator.predict([noise, label])
 
-        # Load the dataset
-        (X_train, y_train), (_, _) = read_mnist("../ganData/mnist.npz")
+    valid = np.ones((half_batch, 1))
+    fake = np.zeros((half_batch, 1))
 
-        # Rescale -1 to 1
-        X_train = (X_train.astype(np.float32) - 127.5) / 127.5
-        X_train = np.expand_dims(X_train, axis=3)
-        y_train = y_train.reshape(-1, 1)
+    d_loss_real = Discriminator.train_on_batch([image, label], valid)
+    d_loss_fake = Discriminator.train_on_batch([generate_image, label], fake)
+    d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
-        half_batch = int(batch_size / 2)
+    noise = np.random.normal(0, 1, (BATCH_SIZE, 100))
 
-        for epoch in range(epochs):
+    valid = np.ones((BATCH_SIZE, 1))
 
-            # ---------------------
-            #  Train Discriminator
-            # ---------------------
+    sampled_labels = np.random.randint(0, 10, BATCH_SIZE).reshape(-1, 1)
 
-            # Select a random half batch of images
-            idx = np.random.randint(0, X_train.shape[0], half_batch)
-            imgs, labels = X_train[idx], y_train[idx]
+    # Train the generator
+    g_loss = Gan.train_on_batch([noise, sampled_labels], valid)
+    proBar.show(d_loss[0], 100*d_loss[1], g_loss)
+    if epoch % 100 == 0:
+        save_images(Generator)
 
-            noise = np.random.normal(0, 1, (half_batch, 100))
-
-            # Generate a half batch of new images
-            gen_imgs = self.generator.predict([noise, labels])
-
-            valid = np.ones((half_batch, 1))
-            fake = np.zeros((half_batch, 1))
-
-            # Train the discriminator
-            d_loss_real = self.discriminator.train_on_batch([imgs, labels], valid)
-            d_loss_fake = self.discriminator.train_on_batch([gen_imgs, labels], fake)
-            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
-
-            # ---------------------
-            #  Train Generator
-            # ---------------------
-
-            noise = np.random.normal(0, 1, (batch_size, 100))
-
-            valid = np.ones((batch_size, 1))
-            # Generator wants discriminator to label the generated images as the intended
-            # digits
-            sampled_labels = np.random.randint(0, 10, batch_size).reshape(-1, 1)
-
-            # Train the generator
-            g_loss = self.combined.train_on_batch([noise, sampled_labels], valid)
-
-            # Plot the progress
-            print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
-
-            # If at save interval => save generated image samples
-            if epoch % save_interval == 0:
-                self.save_imgs(epoch)
-
-    def save_imgs(self, epoch):
-        r, c = 2, 5
-        noise = np.random.normal(0, 1, (r * c, 100))
-        sampled_labels = np.arange(0, 10).reshape(-1, 1)
-
-        gen_imgs = self.generator.predict([noise, sampled_labels])
-
-        # Rescale images 0 - 1
-        gen_imgs = 0.5 * gen_imgs + 0.5
-
-        fig, axs = plt.subplots(r, c)
-        fig.suptitle("CGAN: Generated digits", fontsize=12)
-        cnt = 0
-        for i in range(r):
-            for j in range(c):
-                axs[i,j].imshow(gen_imgs[cnt,:,:,0], cmap='gray')
-                axs[i,j].set_title("Digit: %d" % sampled_labels[cnt])
-                axs[i,j].axis('off')
-                cnt += 1
-        fig.savefig("output/%d.png" % epoch)
-        plt.close()
-
-
-if __name__ == '__main__':
-    cgan = CGAN()
-    cgan.train(epochs=10000, batch_size=32, save_interval=200)
+Generator.save("output/keras_mnist_generator.h5")
--- 07.03.cGAN/readme.md
+++ 07.03.cGAN/readme.md
@@ -8,7 +8,9 @@ Loss的变化如下图所示:
 ![images](results/MNIST_cDCGAN_train_hist.png)
 
 Keras版本的结果如下<br/>
-![images](results/keras_mnist_9800.png)<br/>
+![images](results/keras_mnist_10000.png)<br/>
 ![images](results/keras_mnist.gif)
+
+生成模型h5文件文件为keras_mnist_generator_cdan.h5,在[百度云盘](https://pan.baidu.com/s/1JWLMbibaH1yGZKcIvyT4hQ#list/path=%2F%E6%A8%A1%E5%9E%8B)
 
 # 2.
